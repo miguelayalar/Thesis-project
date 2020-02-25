8d198d972cb03b0491cced8fc48a8199e78e4f6d
Miguel Ayala <miguel.ayala.au@gmail.com>
2020-02-26 10:49:54 +1100

    Creating summary statistics and Lag order selection

--- data cleaning.R
+++ data cleaning.R
@@ -2,10 +2,22 @@
 library(tsibble)
 library(tidyverse)
 library(dplyr)
+library(hexView)
+library(nlWaldTest)
+library(broom)
+library(dynlm)
+library(kableExtra)
+library(zoo)
+library(lmtest)
+library(urca)
+library(tseries)
+library(CADFtest)
+library(strucchange)
+library(vars)
+
 
 
 ## Data wrangling
-
 raw_data <- read.csv('raw_data_ECU.csv')
 
 data <- raw_data %>% 
@@ -16,15 +28,36 @@ data <- raw_data %>%
          ln_lp = log(l_share)) %>% 
   dplyr::select(Year, real_gdp, openness, invest, hcapital, ln_lp, fin_dev)
 
-data <- as_tsibble(data, index = Year)
+data <- as_tsibble(data, index = Year) #%>% column_to_rownames(var = 'Year')
 data %>% summary()
 
 #data observation
-data %>% autoplot(real_gdp)
-data %>% autoplot(openness)
-data %>% autoplot(invest)
-data %>% autoplot(hcapital)
-data %>% autoplot(ln_lp)
-data %>% autoplot(fin_dev)
+data %>% 
+  gather(key = Var, value  = value,
+         real_gdp, openness, invest, hcapital, ln_lp, fin_dev) %>% 
+  ggplot(aes(x = Year, y = value)) +
+  geom_line() +
+  facet_wrap(~ Var, scales = "free_y", nrow = 3) +
+  theme_minimal() + 
+  scale_color_brewer(palette = "Dark2") + 
+  theme(legend.position = "bottom", legend.title = element_blank()) +
+  ylab("")
+  
+
+#unit root test for each series to indicate the level of each series
+#real gdp
+ggtsdisplay(data$real_gdp) # it has a trend, need to check if deterministic or stochastic
+
+dcmp <- data %>%
+  dplyr::select(Year, real_gdp) %>% 
+  model(STL(real_gdp))
+components(dcmp)
 
 
+#VAR Lag order selection criteria 
+VARselect(data, type = "both")
+
+var1 <- VAR(data, p=1, type="const")
+serial.test(var1, lags.pt=10, type="PT.asymptotic")
+var2 <- VAR(data, p=2, type="const")
+serial.test(var2, lags.pt=10, type="PT.asymptotic")
